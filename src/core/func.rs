6cba84e395b07c756dd431c8aa311612cfd21c05
Clivern <hello@clivern.com>
2021-02-06 20:35:07 +0100

    wip

---
+++ src/core/func.rs
@@ -0,0 +1,27 @@
+// Copyright 2021 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+// Pass by refernce
+pub fn double1(x: &mut [i32; 10]) {
+    for i in 0..10 {
+        x[i] += 2;
+    }
+}
+
+// Pass by value
+pub fn double2(x: [i32; 10]) -> [i32; 10] {
+    let mut y: [i32; 10] = x;
+    for i in 0..10 {
+        y[i] += 2;
+    }
+    y
+}
+
+// Pass by value
+pub fn double3(mut x: [i32; 10]) -> [i32; 10] {
+    for i in 0..10 {
+        x[i] += 2;
+    }
+    x
+}
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,38 @@ mod networking {
     pub mod tcp;
 }
 
-// import util
+mod core {
+    pub mod func;
+}
+
 mod util {
     pub mod environ;
     pub mod io;
 }
 
 fn main() {
+    //
+    // Networking
+    //
     // Single Threaded Tcp Server
     // let server = networking::tcp::SingleThreadedTcp::new("127.0.0.1:8000");
     // server.listen();
     println!("Seal!");
+
+    //
+    // Core
+    //
+    // let mut ar: [i32; 10] = [3; 10];
+    // core::func::double1(&mut ar);
+    // println!("{:?}", ar);
+
+    // println!("{:?}", core::func::double2(ar));
+    // println!("{:?}", core::func::double3(ar));
+
+    // let mut a: i32 = 10;
+    // let p: &mut i32 = &mut a;
+
+    //*p = 40;
+
+    // println!("{}", p);
 }
