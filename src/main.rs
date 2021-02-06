bcf981b885b32f029ff573fad721c62259e4c69e
Clivern <hello@clivern.com>
2021-02-06 14:14:22 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
-mod tcp {
+mod networking {
     pub mod tcp;
 }
 
@@ -14,7 +14,7 @@ mod util {
 
 fn main() {
     // Single Threaded Tcp Server
-    // let server = tcp::tcp::SingleThreadedTcp::new("127.0.0.1:8000");
+    // let server = networking::tcp::SingleThreadedTcp::new("127.0.0.1:8000");
     // server.listen();
     println!("Seal!");
 }
