5e537b3f5c510f8e307aa3ed64a3c64c7c37e5dd
Clivern <hello@clivern.com>
2021-02-07 22:46:19 +0100

    wip

--- src/core/func.rs
+++ src/core/func.rs
@@ -30,3 +30,25 @@ pub fn double3(mut x: [i32; 10]) -> [i32; 10] {
 pub fn multiply(x: i32, y: i32) -> i32 {
     x * y
 }
+
+// sum
+pub fn sum<T: std::ops::Add<Output = T>>(n1: T, n2: T) -> T {
+    n1 + n2
+}
+
+// max
+pub fn max<T: std::cmp::PartialOrd>(n1: T, n2: T) -> T {
+    if n1 > n2 {
+        n1
+    } else {
+        n2
+    }
+}
+
+#[test]
+fn test_func() {
+    assert_eq!(5, sum(2, 3));
+    assert_eq!(6, sum(3, 3));
+    assert_eq!(3, max(2, 3));
+    assert_eq!(3, max(3, 3));
+}
